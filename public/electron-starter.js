11727a97bd4cb5245384f6a66ed9534143e983d3
samylegalloudec <samy.legalloudec@gmail.com>
2019-06-12 10:58:23 +0200

    Correction du conflit avec le merge de master

--- public/electron-starter.js
+++ public/electron-starter.js
@@ -40,24 +40,8 @@ function createWindow() {
     });
 }
 
-<<<<<<< HEAD:src/electron-starter.js
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', function(){
-    createWindow();
-    // ExcelServices.createCsv();
-
-    // ExcelServices.getParticipants(function(res){
-    //     console.log("RESULTAT : ", res);
-    // });
-    // ExcelServices.editNumberParticipantByNumber(5, 10);
-
-
-=======
 app.on('ready', () => {
     createWindow();
->>>>>>> master:public/electron-starter.js
 });
 
 // Quit when all windows are closed.
